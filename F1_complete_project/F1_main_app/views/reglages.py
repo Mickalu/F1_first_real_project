7785e80be8deb4391f09f4ee7a8328c264bcb5c4
Mickalu <lucasmichaeli95@gmail.com>
2021-05-03 17:10:25 +0200

    front reglage's pages almost finished

--- F1_complete_project/F1_main_app/views/reglages.py
+++ F1_complete_project/F1_main_app/views/reglages.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from F1_main_app.models import Grand_Prix, Driver, Team
+from F1_main_app.forms.forms import Grand_Prix_form
 
 def reglages(request):
         return render(request, 'reglages/reglage.html')
 
 def reglages_drivers(request):
-        return render(request, 'reglages/load_ajax/reglage_drivers.html')
+        context = {'drivers': Driver.objects.all()}
+        return render(request, 'reglages/load_ajax/reglage_drivers.html', context)
 
 def reglages_teams(request):
-        return render(request, 'reglages/load_ajax/reglage_teams.html')
+        context = {'teams': Team.objects.all()}
+        return render(request, 'reglages/load_ajax/reglage_teams.html', context)
 
 def reglages_grandprixs(request):
-        return render(request, 'reglages/load_ajax/reglage_grandprixs.html')
+        context = {'gps': Grand_Prix.objects.all()}
+        return render(request, 'reglages/load_ajax/reglage_grandprixs.html', context)
+
+
