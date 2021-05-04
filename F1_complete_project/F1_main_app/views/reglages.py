81dd1ceec7fe80447e5b35d3a6d207bbc572041f
Mickalu <lucasmichaeli95@gmail.com>
2021-05-04 11:31:19 +0200

    connection form data to view driver done

--- F1_complete_project/F1_main_app/views/reglages.py
+++ F1_complete_project/F1_main_app/views/reglages.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from F1_main_app.models import Grand_Prix, Driver, Team
 from F1_main_app.forms.forms import Grand_Prix_form
+from django.http import JsonResponse
 
 def reglages(request):
         return render(request, 'reglages/reglage.html')
 
 def reglages_drivers(request):
-        context = {'drivers': Driver.objects.all()}
+        
+        if request.POST.get('action') == 'add_driver':
+                return add_driver(request)
+
+        drivers = Driver.objects.all()
+        
+        context = locals() #get var on the top, here drivers
         return render(request, 'reglages/load_ajax/reglage_drivers.html', context)
+
+def add_driver(request):
+        response_data = {}
+
+
+        
+        return JsonResponse(response_data)
 
 def reglages_teams(request):
         context = {'teams': Team.objects.all()}
